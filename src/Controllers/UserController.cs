217f146b528a4ff7db815044b92eb66b1d52c84b
Alyssa <alyssacomstock95@gmail.com>
2026-02-06 16:39:51 -0800

    clean up

--- src/Controllers/UserController.cs
+++ src/Controllers/UserController.cs
@@ -120,52 +120,5 @@ namespace User.Controllers
                 return StatusCode(500, $"Internal server error: {ex.Message}");
             }
         }
-
-        // Login
-        // [HttpPost("login")]
-        // public async Task<ActionResult<AuthLoginResponse>> Login([FromBody] AuthLoginRequest request)
-        // {
-        //     try
-        //     {
-        //         var response = await _userService.LoginAsync(request);
-        //         return Ok(response);
-
-        //     }
-        //     catch (ArgumentException ex)
-        //     {
-        //         return BadRequest(ex.Message);
-        //     }
-        //     catch (KeyNotFoundException ex)
-        //     {
-        //         return NotFound(ex.Message);
-        //     }
-        //     catch (Exception ex)
-        //     {
-        //         return StatusCode(500, $"Internal server error: {ex.Message}");
-        //     }
-        // }
-
-        // // logout
-        // [HttpPost("logout")]
-        // public async Task<ActionResult> Logout([FromBody] LogoutRequest request)
-        // {
-        //     try
-        //     {
-        //         await _userService.LogoutAsync(request);
-        //         return NoContent();
-        //     }
-        //     catch (ArgumentException ex)
-        //     {
-        //         return BadRequest(ex.Message);
-        //     }
-        //     catch (KeyNotFoundException ex)
-        //     {
-        //         return NotFound(ex.Message);
-        //     }
-        //     catch (Exception ex)
-        //     {
-        //         return StatusCode(500, $"Internal server error: {ex.Message}");
-        //     }
-        // }
     }
 }
